4508d4c95e97cfdd66fae3b6111fe113f3d99386
Achanandhi24 <achanandhi.m@gmail.com>
2023-03-27 21:12:17 +0530

    new commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,13 @@ import React from "react";
 import Button from '@mui/material/Button';
 import Navbar from "../Components/Layout/Navbar"
 import { Box } from "@mui/material";
-function About(){
+function Home(){
     return(
         <>
         <Navbar />
         <h1>Hi this is Achanandhi!</h1>
-     
+        <h1>Computer Science Student</h1>
         </>
     )
 }
-export default About;
+export default Home;
